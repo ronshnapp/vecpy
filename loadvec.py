fixed more bugs regarding the differentiation of vectors after rotation.

loadvec.py
@@ -55,6 +55,7 @@ def vecToMatrix(data):
     v1 = reshape(data[:,3],shape(Y))*-1
     chc = reshape(data[:,4],shape(X))  
     return (X,Y,u1,v1,chc)
+
     
 def genQuiver(vec):
     """
@@ -78,9 +79,16 @@ def fit2LLK(vec):
 def genVelHist(vec):
     """
     this function will plot a normalized histogram of
-    the velocity data
+    the velocity data. the velocity in the histpgram 
+    contains all the data set such that CHC == 1
     """
-    u,v = vec.U.flatten() , vec.V.flatten()
+    u,v,chc = vec.U.flatten() , vec.V.flatten(), vec.CHC.flatten()
+    print shape(u),shape(v),shape(chc)
+    u1, v1 = [], []
+    for i in range(size(chc)):
+        if chc[i]==1:
+            u1.append(u[i])
+            v1.append(v[i])
     ax1 = plt.subplot2grid((2,1),(0,0))
     ax1.hist(u,bins=np.sqrt(len(u))*0.5,normed=1)
     ax1.set_xlabel('u [mm/sec]')
@@ -91,10 +99,11 @@ def genVelHist(vec):
     return
     
 def genVorticityMap(vec):
-    dUy = gradient(vec.U)[1]
-    dVx = gradient(vec.V)[0]
-    dx = gradient(vec.X)[0]
-    dy = gradient(vec.Y)[1]
+    dUy = gradient(vec.U)[0]*cos(vec.theta)-gradient(vec.U)[1]*sin(vec.theta)
+    dVx = gradient(vec.V)[1]*cos(vec.theta)+gradient(vec.V)[0]*sin(vec.theta)
+    dx = gradient(vec.X)[1]*cos(vec.theta)+gradient(vec.X)[0]*sin(vec.theta)
+    dy = gradient(vec.Y)[0]*cos(vec.theta)-gradient(vec.Y)[1]*sin(vec.theta)
+    print amax(dUy),amax(dVx),amax(dx),amax(dy)
     vorticity = dVx/dy-dUy/dx
     plt.contourf(vec.X,vec.Y,vorticity)
     plt.xlabel('x ['+vec.length+']')