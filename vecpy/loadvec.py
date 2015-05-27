639a4990d80368063214874fa3c17385f0efd205
Alex Liberzon <alex.liberzon@gmail.com>
2015-05-27 21:56:42 +0300

    added get_data_openpiv to read from the .txt files ( no header, space, 5 columns) added tests/test_openpiv.py $ nosetests passes

--- vecpy/loadvec.py
+++ vecpy/loadvec.py
@@ -16,10 +16,12 @@ from vecpy import vecClass
 def get_dt(fname,path):
     """given a .vec file this will return the delta t 
     from the file in micro seconds"""
-    os.chdir(path)
-    f = open(fname)
-    header = f.readline()
-    f.close()
+    # os.chdir(path) BUG
+    fname = os.path.join(os.path.abspath(path),fname) # just make a full path name 
+    # new way of opening and closing the file
+    with open(fname) as f:
+        header = f.readline()
+        
     ind1 = header.find('MicrosecondsPerDeltaT')
     dt = float(header[ind1:].split('"')[1])
     return dt
@@ -27,10 +29,23 @@ def get_dt(fname,path):
 def get_data(fname,path):
     """this function gathers and retuens the data found in
     a single .vec file"""
-    
-    # os.chdir(path)
     fname = os.path.join(os.path.abspath(path),fname) # just make a full path name 
-    data = np.genfromtxt(fname,skip_header=1,delimiter=',',usecols=(0,1,2,3,4))
+    if lower(fname).endswith('.vec'):
+        data = np.genfromtxt(fname,skip_header=1,delimiter=',',usecols=(0,1,2,3,4))
+    else:
+        raise 'Wrong file extension'
+        
+    return data
+    
+def get_data_openpiv(fname,path):
+    """this function gathers and retuens the data found in
+    a single .txt file"""
+    fname = os.path.join(os.path.abspath(path),fname)
+    if fname.endswith('.txt'): 
+        data = np.genfromtxt(fname,usecols=(0,1,2,3,4))
+    else:
+        raise 'Wrong file extension'
+    
     return data
 	
 def read_directory(dirname):
@@ -56,7 +71,8 @@ def vecToMatrix(data):
     X,Y = meshgrid(x,y)
     u1 = reshape(data[:,2],shape(X))
     v1 = reshape(data[:,3],shape(Y))*-1
-    chc = reshape(data[:,4],shape(X)) 
+    chc = reshape(data[:,4],shape(X))
+    
     return (X,Y,u1,v1,chc)
 
 def vecToVec(data):
