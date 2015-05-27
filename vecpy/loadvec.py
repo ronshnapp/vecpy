f554a2afa413273ea33f773a04166209b4925557
Alex Liberzon <alex.liberzon@gmail.com>
2015-05-27 20:38:46 +0300

    init.py is now in /tests/test0.py graphics moved to plotting file

--- vecpy/loadvec.py
+++ vecpy/loadvec.py
@@ -58,58 +58,10 @@ def vecToMatrix(data):
     chc = reshape(data[:,4],shape(X))  
     return (X,Y,u1,v1,chc)
 
-    
-def genQuiver(vec):
-    """
-    this function will generate a quiver plot from a vec 
-    object    
-    """
-    mpl.contourf(vec.X,vec.Y,sqrt(vec.U**2+vec.V**2),alpha=0.3)
-    cbar = mpl.colorbar()
-    cbar.set_label(r'Velocity [m $\cdot$ s$^{-1}$]')
-    mpl.quiver(vec.X,vec.Y,vec.U,vec.V,scale=2000)
-    mpl.xlabel('x ['+vec.length+']')
-    mpl.ylabel('y ['+vec.length+']')
-    return
-    
+        
 def fit2LLK(vec):
         """this function is specialy made for Ron's 
         LLK PIV experiment"""
         vec.rotate(-90)
         return
     
-def genVelHist(vec):
-    """
-    this function will plot a normalized histogram of
-    the velocity data. the velocity in the histpgram 
-    contains all the data set such that CHC == 1
-    """
-    u,v,chc = vec.U.flatten() , vec.V.flatten(), vec.CHC.flatten()
-    print shape(u),shape(v),shape(chc)
-    u1, v1 = [], []
-    for i in range(size(chc)):
-        if chc[i]==1:
-            u1.append(u[i])
-            v1.append(v[i])
-    ax1 = plt.subplot2grid((2,1),(0,0))
-    ax1.hist(u,bins=np.sqrt(len(u))*0.5,normed=1)
-    ax1.set_xlabel('u [mm/sec]')
-    ax2 = plt.subplot2grid((2,1),(1,0))
-    ax2.hist(v,bins=np.sqrt(len(v)*0.5),normed=1)
-    ax2.set_xlabel('v [mm/sec]')
-    plt.tight_layout()
-    return
-    
-def genVorticityMap(vec):
-    dUy = gradient(vec.U)[0]*cos(vec.theta)-gradient(vec.U)[1]*sin(vec.theta)
-    dVx = gradient(vec.V)[1]*cos(vec.theta)+gradient(vec.V)[0]*sin(vec.theta)
-    dx = gradient(vec.X)[1]*cos(vec.theta)+gradient(vec.X)[0]*sin(vec.theta)
-    dy = gradient(vec.Y)[0]*cos(vec.theta)-gradient(vec.Y)[1]*sin(vec.theta)
-    print amax(dUy),amax(dVx),amax(dx),amax(dy)
-    vorticity = dVx/dy-dUy/dx
-    plt.contourf(vec.X,vec.Y,vorticity)
-    plt.xlabel('x ['+vec.length+']')
-    plt.ylabel('y ['+vec.length+']')
-    cbar = plt.colorbar()
-    cbar.set_label(r'Vorticity [s$^{-1}$]')
-    return
