743183fe1fe3c18aa333f300c0a37623c7bef0bd
Ron Shnapp <ronshnapp@mail.tau.ac.il>
2016-01-24 09:44:20 +0200

    some import fixes added autmatic aspect ratio for some of the plots

--- vecPy/vecPlot.py
+++ vecPy/vecPlot.py
@@ -9,6 +9,9 @@ velocity attributes of the .vec files that were open.
 use this code over vec objects.
 """
 
+from numpy import linspace, amax, sqrt, gradient, cos, sin
+from matplotlib.pyplot import colorbar, get_cmap, subplots, contourf
+
 import numpy as np
 from numpy import *
 from matplotlib.pylab import *
@@ -20,7 +23,8 @@ plt.rcParams['animation.ffmpeg_path'] = 'C:/ffmpeg/bin/ffmpeg.exe'
 from scipy.ndimage.filters import median_filter
 
 def genQuiver(vec, arrScale = 25.0, threshold = None, nthArr = 1, 
-              contourLevels = None, colorbar = True, logscale = False):
+              contourLevels = None, colbar = True, logscale = False,
+              aspectratio='equal'):
     """
     Generates a quiver plot of a 'vec' object
     Inputs:   
@@ -30,6 +34,7 @@ def genQuiver(vec, arrScale = 25.0, threshold = None, nthArr = 1,
         contourLevels - use to specify the maximum value (abs) of contour plots 
         colbar - True/False wether to generate a colorbar or not
         logscale - if true then colorbar is on log scale
+        aspectratio - set auto or equal for the plot's apearence
     Outputs:
         none
     Usage:
@@ -44,27 +49,30 @@ def genQuiver(vec, arrScale = 25.0, threshold = None, nthArr = 1,
         
     S = sqrt(u**2 + v**2)
     
+    f, ax = subplots()     
+    
     if contourLevels is None:
-        levels = np.linspace(0, amax(S), 30) # default contour levels up to max of S
+        levels = linspace(0, amax(S), 30) # default contour levels up to max of S
     else:
-        levels = np.linspace(0, contourLevels, 30)
+        levels = linspace(0, contourLevels, 30)
     if logscale:
-        mpl.contourf(vec.x,vec.y,S,alpha=0.8,
-                 cmap=plt.cm.get_cmap("Blues"), 
+        c = ax.contourf(vec.x,vec.y,S,alpha=0.8,
+                 cmap = get_cmap("Blues"), 
                  levels=levels, norm=matplotlib.colors.LogNorm())
     else:
-        mpl.contourf(vec.x,vec.y,S,alpha=0.8,
-                 cmap=plt.cm.get_cmap("Blues"), 
+        c = ax.contourf(vec.x,vec.y,S,alpha=0.8,
+                 cmap = get_cmap("Blues"), 
                  levels=levels)
-    if colorbar:
-        cbar = mpl.colorbar()
+    if colbar:
+        cbar = colorbar(c)
         cbar.set_label(r'$\left| \, V \, \right|$ ['+vec.lUnits+' $\cdot$ '+vec.tUnits+'$^{-1}$]')
     n = nthArr
-    mpl.quiver(vec.x[1::n,1::n],vec.y[1::n,1::n],
+    ax.quiver(vec.x[1::n,1::n],vec.y[1::n,1::n],
                u[1::n,1::n],v[1::n,1::n],units='width',
                scale=amax(S*arrScale),headwidth=2 )
-    mpl.xlabel('x [' + vec.lUnits + ']')
-    mpl.ylabel('y [' + vec.lUnits + ']')
+    ax.set_xlabel('x [' + vec.lUnits + ']')
+    ax.set_ylabel('y [' + vec.lUnits + ']')
+    ax.set_aspect(aspectratio)
     
     
 def genFluctuationQuiver(vec):
@@ -75,11 +83,11 @@ def genFluctuationQuiver(vec):
     vec.getVelStat()
     u,v = vec.u-vec.Umean,vec.v-vec.Vmean
     S = sqrt(u**2+v**2)
-    levels = np.linspace(0, amax(S), 30)
-    mpl.contourf(vec.x,vec.y,S,alpha=0.5,
-                 cmap=plt.cm.get_cmap("Greens"),
+    levels = linspace(0, amax(S), 30)
+    contourf(vec.x,vec.y,S,alpha=0.5,
+                 cmap = get_cmap("Greens"),
                  levels=levels)
-    cbar = mpl.colorbar()
+    cbar = colorbar()
     cbar.set_label(r'Velocity [m $\cdot$ s$^{-1}$]')
     mpl.quiver(vec.x, vec.y, u, v, units='width',scale=amax(sqrt(u**2+v**2))*25.0,headwidth=2 )
     mpl.xlabel('x [' + vec.lUnits + ']')
@@ -92,17 +100,18 @@ def genVelHist(vec):
     the velocity data.
     """
     u1, v1 = vec.u.flatten(), vec.v.flatten()
-    ax1 = plt.subplot2grid((2,1),(0,0))
-    ax1.hist(u1,bins=np.sqrt(len(u1))*0.5,normed=1)
-    ax1.set_xlabel('u [mm/sec]')
+    f,ax = subplots(2)
+    ax1,ax2 = ax
+    ax1.hist(u1,bins=sqrt(len(u1))*0.5,normed=1)
+    ax1.set_xlabel('u ['+vec.velUnits+']')
     ax2 = plt.subplot2grid((2,1),(1,0))
-    ax2.hist(v1,bins=np.sqrt(len(v1)*0.5),normed=1)
-    ax2.set_xlabel('v'+vec.velUnits)
+    ax2.hist(v1,bins=sqrt(len(v1)*0.5),normed=1)
+    ax2.set_xlabel('v ['+vec.velUnits+']')
     plt.tight_layout()
     
     
 def genVorticityMap(vec, threshold = None, contourLevels = None, 
-                    colorbar = True,  logscale = False):
+                    colbar = True,  logscale = False, aspectration='equal'):
     """ why do we rotate the vector before taking derivative? """
     # BUG:
     dUy = gradient(vec.u)[0]*cos(vec.theta)-gradient(vec.u)[1]*sin(vec.theta)
@@ -110,58 +119,67 @@ def genVorticityMap(vec, threshold = None, contourLevels = None,
     dx = gradient(vec.x)[1]*cos(vec.theta)+gradient(vec.x)[0]*sin(vec.theta)
     dy = gradient(vec.y)[0]*cos(vec.theta)-gradient(vec.y)[1]*sin(vec.theta)
     vorticity = dVx/dy-dUy/dx
+    
+    f,ax = subplots()    
+    
     if threshold != None:
         vorticity = thresholdArray(vorticity,threshold)
     m = amax(absolute(vorticity))
     if contourLevels == None:
-        levels = np.linspace(-m, m, 30)
+        levels = linspace(-m, m, 30)
     else:
-        levels = np.linspace(-contourLevels, contourLevels, 30)
+        levels = linspace(-contourLevels, contourLevels, 30)
         
     if logscale:
-        plt.contourf(vec.x,vec.y,np.abs(vorticity), levels=levels,
-                 cmap = plt.cm.get_cmap('RdYlBu'), norm=matplotlib.colors.LogNorm())
+        c = ax.contourf(vec.x,vec.y,np.abs(vorticity), levels=levels,
+                 cmap = get_cmap('RdYlBu'), norm=matplotlib.colors.LogNorm())
     else:
-        plt.contourf(vec.x,vec.y,vorticity, levels=levels,
-                 cmap = plt.cm.get_cmap('RdYlBu'))
+        c = ax.contourf(vec.x,vec.y,vorticity, levels=levels,
+                 cmap = get_cmap('RdYlBu'))
     plt.xlabel('x [' + vec.lUnits + ']')
     plt.ylabel('y [' + vec.lUnits + ']')
-    if colorbar:
-        cbar = plt.colorbar()
+    if colbar:
+        cbar = colorbar(c)
         cbar.set_label(r'$\omega$ [s$^{-1}$]')
+    ax.set_aspect(aspectration)
 
 
 def genShearMap(vec, threshold = None, contourLevels = None, logscale = False,
-                colorbar = True):
+                colbar = True, aspectratio='equal'):
     """this function plots a map of the xy strain e_xy"""
     dUy = gradient(vec.u)[0]*cos(vec.theta)-gradient(vec.u)[1]*sin(vec.theta)
     dVx = gradient(vec.v)[1]*cos(vec.theta)+gradient(vec.v)[0]*sin(vec.theta)
     dx = gradient(vec.x)[1]*cos(vec.theta)+gradient(vec.x)[0]*sin(vec.theta)
     dy = gradient(vec.y)[0]*cos(vec.theta)-gradient(vec.y)[1]*sin(vec.theta)
     strain = dVx/dy+dUy/dx
+    
+    f, ax = subplots()    
+    
     if threshold != None:
         strain = thresholdArray(strain,threshold)
     m = amax(absolute(strain))
     if contourLevels == None:
-        levels = np.linspace(-m, m, 30)
+        levels = linspace(-m, m, 30)
     else:
-        levels = np.linspace(-contourLevels, contourLevels, 30)
+        levels = linspace(-contourLevels, contourLevels, 30)
         
     if logscale:
-        plt.contourf(vec.x,vec.y,np.abs(strain), levels=levels,
-                 cmap = plt.cm.get_cmap('PRGn'), norm=matplotlib.colors.LogNorm())
+        c = ax.contourf(vec.x,vec.y,np.abs(strain), levels=levels,
+                 cmap = get_cmap('PRGn'), norm=matplotlib.colors.LogNorm())
     else:
-        plt.contourf(vec.x,vec.y,strain, levels=levels,
-                 cmap = plt.cm.get_cmap('PRGn'))
+        c = ax.contourf(vec.x,vec.y,strain, levels=levels,
+                 cmap = get_cmap('PRGn'))
     plt.xlabel('x [' + vec.lUnits + ']')
     plt.ylabel('y [' + vec.lUnits + ']')
-    if colorbar:
-        cbar = plt.colorbar()
+    if colbar:
+        cbar = colorbar(c)
         cbar.set_label(r'$\epsilon_t$ [s$^{-1}$]')
+    ax.set_aspect(aspectratio)
     
 
 def genFlowAcceleration(vec, arrScale = 25.0, threshold = None, nthArr = 1,
-                        contourLevels = None, logscale = False, colorbar=True):
+                        contourLevels = None, logscale = False, 
+                        colbar=True, aspectratio='equal'):
     """this function will plot a contour plot of
     the convective term of material derivative.
     i.e. it plots the magnitude of the vector 
@@ -179,19 +197,20 @@ def genFlowAcceleration(vec, arrScale = 25.0, threshold = None, nthArr = 1,
         ay = thresholdArray(ay,threshold)
     S = sqrt(ax**2+ay**2)
     if contourLevels == None:
-        levels = np.linspace(0, amax(S), 30)
+        levels = linspace(0, amax(S), 30)
     else:
-        levels = np.linspace(0, contourLevels, 30)
+        levels = linspace(0, contourLevels, 30)
+    f,axs = subplots()
     if logscale:
-        mpl.contourf(vec.x,vec.y,S,alpha=0.5,
-                 cmap=plt.cm.get_cmap("OrRd"), 
+        c = axs.contourf(vec.x,vec.y,S,alpha=0.5,
+                 cmap = get_cmap("OrRd"), 
                  levels=levels, norm=matplotlib.colors.LogNorm())
     else:
-        mpl.contourf(vec.x,vec.y,S,alpha=0.5,
-                 cmap=plt.cm.get_cmap("OrRd"), 
+        c = axs.contourf(vec.x,vec.y,S,alpha=0.5,
+                 cmap = get_cmap("OrRd"), 
                  levels=levels)
-    if colorbar:
-        cbar = mpl.colorbar()
+    if colbar:
+        cbar = colorbar(c)
         cbar.set_label(r'$\left| \, \left( V\cdot \nabla \right) \cdot V \, \right|$ ['+
                    vec.lUnits+' $\cdot$ '+vec.tUnits+'$^{-2}$]')
     n = nthArr
@@ -200,18 +219,18 @@ def genFlowAcceleration(vec, arrScale = 25.0, threshold = None, nthArr = 1,
                scale=amax(S)*arrScale,headwidth=2 )
     mpl.xlabel('x [' + vec.lUnits + ']')
     mpl.ylabel('y [' + vec.lUnits + ']')
-    
+    axs.set_aspect(aspectratio)
     
 def animateVecList(vecList, arrowscale=1, savepath=None):
     X, Y = vecList[0].x, vecList[0].y
     U, V = vecList[0].u, vecList[0].v
-    fig, ax = plt.subplots(1,1)
+    fig, ax = subplots(1,1)
     #Q = ax.quiver(X, Y, U, V, units='inches', scale=arrowscale)
     M = sqrt(pow(U, 2) + pow(V, 2))    
     Q = ax.quiver(X[::3,::3], Y[::3,::3], 
                   U[::3,::3], V[::3,::3], M[::3,::3],
                  units='inches', scale=arrowscale)
-    cb = plt.colorbar(Q)
+    cb = colorbar(Q)
     cb.ax.set_ylabel('velocity ['+vecList[0].lUnits+'/'+vecList[0].tUnits+']')
     text = ax.text(0.2,1.05, '1/'+str(len(vecList)), ha='center', va='center', transform=ax.transAxes)
     def update_quiver(num,Q,vecList,text):
